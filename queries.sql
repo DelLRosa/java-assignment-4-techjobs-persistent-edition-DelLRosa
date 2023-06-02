f91b0220a7c9d646a8b14bbfaa7ea26ebad09254
Delaney <delaney.rosa1@gmail.com>
2023-06-01 19:32:03 -0500

    part one completed. Created/ connected techjobs database

--- queries.sql
+++ queries.sql
@@ -1,5 +1,5 @@
 -- Part 1: Test it with SQL
-
+select column_name, data_type from information_schema.columns where table_scheme = 'techjobs' and table_name='jobs'
 -- Part 2: Test it with SQL
 
 -- Part 3: Test it with SQL
