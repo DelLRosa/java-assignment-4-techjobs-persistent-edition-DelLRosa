2bb3688c1b21c459c6eb581c6d16de28cb3c6da5
Delaney <delaney.rosa1@gmail.com>
2023-06-06 21:26:50 -0500

    Completed Task 2

--- queries.sql
+++ queries.sql
@@ -1,6 +1,7 @@
 -- Part 1: Test it with SQL
 select column_name, data_type from information_schema.columns where table_scheme = 'techjobs' and table_name='jobs'
 -- Part 2: Test it with SQL
+SELECT name FROM employer WHERE location="St. Louis City";
 
 -- Part 3: Test it with SQL
 
