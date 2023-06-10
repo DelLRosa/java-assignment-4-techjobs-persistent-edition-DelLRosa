8f1bd089222b8acc517a9cd26b57534349fff149
Delaney <delaney.rosa1@gmail.com>
2023-06-10 18:10:25 -0500

    All Autograding tests passing

--- src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
@@ -1,7 +1,11 @@
 package org.launchcode.techjobs.persistent.controllers;
 
+import org.launchcode.techjobs.persistent.models.Employer;
 import org.launchcode.techjobs.persistent.models.Job;
+import org.launchcode.techjobs.persistent.models.Skill;
 import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
+import org.launchcode.techjobs.persistent.models.data.JobRepository;
+import org.launchcode.techjobs.persistent.models.data.SkillRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by LaunchCode
@@ -19,11 +24,17 @@ public class HomeController {
 
     @Autowired
     EmployerRepository employerRepository;
+    @Autowired
+    SkillRepository skillRepository;
+
+    @Autowired
+    JobRepository jobRepository;
 
     @RequestMapping("")
     public String index(Model model) {
 
         model.addAttribute("title", "My Jobs");
+        model.addAttribute("jobs", jobRepository.findAll());
 
         return "index";
     }
@@ -32,6 +43,7 @@ public class HomeController {
     public String displayAddJobForm(Model model) {
         model.addAttribute("title", "Add Job");
         model.addAttribute("employers", employerRepository.findAll());
+        model.addAttribute("skills", skillRepository.findAll());
         model.addAttribute(new Job());
         return "add";
     }
@@ -45,15 +57,34 @@ public class HomeController {
             return "add";
         }
 
-        employerRepository.findById(employerId);
+        Employer employer= employerRepository.findById(employerId).orElse(new Employer());
+        newJob.setEmployer(employer);
+
+        List<Skill> skillObjs = (List<Skill>) skillRepository.findAllById(skills);
+        newJob.setSkills(skillObjs);
+
+        System.out.println(skillObjs);
+        System.out.println(employerRepository.findById(employerId));
+        System.out.println(newJob);
+
+//         Optional employerObj=employerRepository.findById(employerId);
+//
+//        newJob.setEmployer((Employer) employerObj.get());
+
+        jobRepository.save(newJob);
 
         return "redirect:";
     }
 
     @GetMapping("view/{jobId}")
     public String displayViewJob(Model model, @PathVariable int jobId) {
-
-        return "view";
+        Optional optJob=jobRepository.findById(jobId);
+        if (!optJob.isEmpty()){
+            Job job = (Job) optJob.get();
+            model.addAttribute("job", job);
+            return "view";
+        }
+        return "redirect:/";
     }
 
 
